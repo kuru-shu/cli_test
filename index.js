3b87576e0f3fe1fa363409286f2744b949a02663
kuru-shu <kurushu71@gmail.com>
2024-11-17 20:18:47 +0900

    雛形作成(5.1)、引数の処理(5.2)

---
+++ index.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const fs = require('fs');
+
+const packageStr = fs.readFileSync(path.resolve(__dirname, 'package.json'), {
+  encoding: 'utf-8',
+});
+const package = JSON.parse(packageStr);
+
+// nameオプションのチェック
+const nameOption = process.argv.includes('--name');
+
+if (nameOption) {
+  console.log(package.name);
+} else {
+  console.log('オプションがありません');
+}
